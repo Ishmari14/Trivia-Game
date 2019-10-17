0b06250f3a6ff3db5c46623671c342ce1e76157c
Jhayla Young <jhayla.young@gmail.com>
2019-10-16 19:46:47 -0500

    hard time with this button function

--- assets/javascript/triviaLogic.js
+++ assets/javascript/triviaLogic.js
@@ -2,67 +2,99 @@
 $(document).ready(function () {
 
     $("#start").on('click', items.start);
-    $(document).on('click', '.options', trivia.guesses);
+    $(document).on('click', '.options', items.guesses);
+    $("#timeleft").hide();
+
+});
+
+var items = {
+
+    //variables for the starting functions//
+    correct = 0,
+    wrong = 0,
+    current = 0,
+    counter = 30,
+    counterID = '',
+    counterON = false,
+
+    ///start function///
+
+    start: function () {
+
+        items.correct = 0;
+        items.wrong = 0;
+        items.current = 0;
+
+        //clear///
+        clearInterval(items.counterID);
+
+        ///show & hide things//
+        $("#game-area").show();
+        $("#result").html('');
+        $('#counter').text(items.counter);
+        $('#start').hide();
+        $('#timeleft').show();
+
+        // next question//
+        items.nextQ();
 
 
 
+    },
 
+    nextQ: function () {
 
+        items.counter = 20;
+        $('#counter').text(items.counter);
+        var sec = 15;
+        var time = setInterval(myTimer, 1000);
 
-
-
-
-    var items = {
-
-        //variables for the starting functions//
-        correct = 0,
-        wrong = 0,
-        current = 0,
-        counter = 30,
-        counterID = '',
-        counterON = false,
-
-        ///start function///
-
-        start: function () {
-
-            items.correct = 0;
-            items.wrong = 0;
-            items.current = 0;
-
-            //clear///
-            clearInterval(items.counterID);
-
-
-
+        function myTimer() {
+            document.getElementById('timer').innerHTML = sec + "sec left";
+            sec--;
+            if (sec == -1) {
+                clearInterval(time);
+                console.log('Time UP!');
+            }
         }
+    },
+    ///set timer///
 
-//questions///
-        question: {
-            q1: "Who is the Avatar of Nyx?",
-            q2: "Who is the ruler of the Velvet Room?",
-            q3: "Where can Mementos be found?",
-            q4: "What does S.E.E.S stand for?",
-            q5: "Who is the serial killer in Persona 4?",
-            q6: "How do you access the Metaverse in Persona 5?",
-            q7: "What causes the strange occurences in Persona 2?",
-            q8: "What is the ultimate personafication of death in Persona 3?",
-            q9: "What is the date for the final battle in Persona 5?",
-            q10: "What tarot card is repreentative of the main character in Persona 2: Innocent Sin?",
+    //questions///
+    question: {
+        q1: "Who is the Avatar of Nyx?",
+        q2: "Who is the ruler of the Velvet Room?",
+        q3: "Where can Mementos be found?",
+        q4: "What does S.E.E.S stand for?",
+        q5: "Who is the serial killer in Persona 4?",
+        q6: "How do you access the Metaverse in Persona 5?",
+        q7: "What causes the strange occurences in Persona 2?",
+        q8: "What is the ultimate personafication of death in Persona 3?",
+        q9: "What is the date for the final battle in Persona 5?",
+        q10: "What tarot card is repreentative of the main character in Persona 2: Innocent Sin?",
 
-        },
+    },
 
-        choices: {
-            q1: ['Ryoji', 'Minato', 'Yukari', 'Ken'],
-            q2: ['Philemon', 'Nyarleothep', 'Igor', 'Nyx'],
-            q3: ['Shibuya Station', 'Nagoya Station', 'Inaba', 'Xibailba'],
-        },
+    choices: {
+        q1: ['Ryoji', 'Minato', 'Yukari', 'Ken'],
+        q2: ['Philemon', 'Nyarleothep', 'Igor', 'Nyx'],
+        q3: ['Shibuya Station', 'Nagoya Station', 'Inaba', 'Xibailba'],
+    },
 
-        answer: {
-            q1: "Ryoji",
-            q2: "Igor",
-            q3: "Shibuya Station",
-        },
+    answer: {
+        q1: "Ryoji",
+        q2: "Igor",
+        q3: "Shibuya Station",
+    },
 
 
-    }
+},
+
+
+
+
+
+
+
+
+
