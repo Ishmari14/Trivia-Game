4a1f7d7b4ac655c8df8764cf072ade0cbc994c99
Jhayla Young <jhayla.young@gmail.com>
2019-10-16 19:57:47 -0500

    i want to cry

--- assets/javascript/triviaLogic.js
+++ assets/javascript/triviaLogic.js
@@ -51,7 +51,7 @@ var items = {
         var time = setInterval(myTimer, 1000);
 
         function myTimer() {
-            document.getElementById('timer').innerHTML = sec + "sec left";
+            document.getElementById('counter').innerHTML = sec + "sec left";
             sec--;
             if (sec == -1) {
                 clearInterval(time);
