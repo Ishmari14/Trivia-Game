f446e27cc2d14b1a5db249ea6c037920d0c99970
Jhayla Young <jhayla.young@gmail.com>
2019-10-20 16:25:03 -0500

    finally got it to start

--- assets/javascript/triviaLogic.js
+++ assets/javascript/triviaLogic.js
@@ -1,32 +1,156 @@
-var setup = false;
+//All elements first/// not to get confused/// again...
 
-function startupPipeline() {
+const start = document.getElementById("start-btn");
+const trivia = document.getElementById("trivia");
+const qImg = document.getElementById("question-img");
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+const progress = document.getElementById("progress");
+const scoreContainer = ("scoreContainer");
 
-    var self = this,
-        ///variables for the game area///
+///questions and answers array///
 
-        area = document.getElementById("area"),
-        startbtn = document.getElementById("start-btn"),
+var questions = [
+    {
+        question: "Who is the Avatar of Nyx",
+        imgSrc: "",
+        choiceA: "Ryoji",
+        choiceB: "Yukari",
+        choiceC: "Minato",
+        choiceD: "Shinjiro",
+        correct: "A"
 
-        title = document.getElementById("title"),
+    },
 
-        ///variables for questions///
+    {
+        question: "",
+        imgSrc: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correct: ""
 
-        questions = document.getElementsByClassName("questions"),
-        score = document.getElementsByClassName("score"),
-        counter = document.getElementsByClassName("counter"),
+    },
 
+    {
+        question: "",
+        imgSrc: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correct: ""
+
+    },
+
+    {
+        question: "",
+        imgSrc: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correct: ""
+
+    },
+
+];
+
+//render for questions///
+
+var lastquestionind = questions.length - 1;
+var runningquestion = 0;
+
+function renderquestions() {
+    var q = questions[runningquestion];
+    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+}
+
+///render counter///
+
+const timequestion = 30; //30 seconds
+const gaugewth = 200;
+var count = 0;
+const gaugeProgress = gaugewth / timequestion;
+
+function counterrnder() {
+    if (count <= timequestion) {
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeProgress + "px";
+        count++;
+    }
+
+    else {
+        count = 0;
+        answerwrong();
+
+        if (runningquestion < lastquestionind) {
+            runningquestion++;
+            questionrnder();
+        }
+
+    }
+}
+
+///start game///
+
+start.onclick = function startGame() {
+    start.style.display = "none";
+    counterrnder();
+    CountDown = setInterval(counterrnder, 1000);
+    questionrnder();
+    trivia.style.display = "block";
+    console.log("start")
 
 
 }
 
 
+function checkAnswer(answer) {
+    if (questions[runningquestion].correct == answer) {
+        score++
+        correctAnswer();
 
+    }
+    else {
+        answerwrong();
+    }
+    if (runningquestion < lastquestionind) {
+        count = 0;
+        runningquestion++;
+        questionrnder();
+    }
+    else {
+        clearInterval(CountDown);
+        scorernder();
+    }
+}
 
+///render score///
 
+function scorernder() {
+    scoreContainer.style.display = "block";
+    var scoreper = Math.round(100 * score / questions.length);
+    var img = (scoreper >= 90) ? "" :
+        (scoreper >= 70) ? "" :
+            (scoreper >= 50) ? "" :
+                (scoreper >= 30) ? "" :
+                    (scoreper >= 10) ? "" : "";
 
+    scoreContainer.innerHTML = "<img src=" + img + "><p>" + scoreper + "%</p>";
 
-
+}
 
 
 
